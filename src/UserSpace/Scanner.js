53b9423a7bc5a09480102d2ce60d322a2fd19ec2
Lily <mxlily.dev@gmail.com>
2021-12-13 15:17:06 +0100

    🚀 DEPLOY | TAKE A PICTURE

--- src/UserSpace/Scanner.js
+++ src/UserSpace/Scanner.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { IconButton} from 'react-native-paper';
 import { RNCamera } from 'react-native-camera'
 
@@ -7,51 +7,66 @@ class Scanner extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <RNCamera
-                    style={styles.camera}
-                    ref={ref => {
-                        this.camera = ref
-                    }}
-                />
-                <View
-                    style={{
-                    position: 'absolute',
-                    bottom: 0,
-                    width: "100%",
-                    backgroundColor: 'transparant',
-                    }}
-                >
-                <IconButton
-                    style={styles.buttonScan}
-                    icon="cube-scan"
-                    color="white"
-                    size={50}
-                    onPress={() => console.log('Pressed')}
-                />
-                </View>
+              <RNCamera
+                style={styles.preview}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.off}
+                androidCameraPermissionOptions={{
+                  title: 'Permission to use camera',
+                  message: 'We need your permission to use your camera',
+                  buttonPositive: 'Ok',
+                  buttonNegative: 'Cancel',
+                }}
+                androidRecordAudioPermissionOptions={{
+                  title: 'Permission to use audio recording',
+                  message: 'We need your permission to use your audio',
+                  buttonPositive: 'Ok',
+                  buttonNegative: 'Cancel',
+                }}
+              >
+                {({ camera, status, recordAudioPermissionStatus }) => {
+                  return (
+                    <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                      <IconButton style={styles.buttonScan}
+                            icon="cube-scan"
+                            color="white"
+                            size={50}
+                            onPress={() => this.takePicture(camera)}>
+                      </IconButton>
+                    </View>
+                  );
+                }}
+              </RNCamera>
             </View>
-        )
-    }
+          );
+        }
+
+takePicture = async function(camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    console.log(data.uri);
+};
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'black'
-    },
-    camera: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    buttonScan: {
-        borderWidth: 0,
-        marginBottom: 100,
-        justifyContent: 'center',
-        alignSelf: 'center',
-        margin: 'auto',
-        backgroundColor: '#8e38ff',
-    },
+container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+},
+preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+},
+buttonScan: {
+    borderWidth: 0,
+    marginBottom: 100,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    margin: 'auto',
+    backgroundColor: '#8e38ff',
+},
 });
 
 export default Scanner
