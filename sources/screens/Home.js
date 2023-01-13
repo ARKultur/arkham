2738b884a689cf5835e0c7b8b387f23803217c4a
Maxime <maxime.same-kotto@epitech.eu>
2023-01-13 22:00:52 +0100

    add google map map

--- sources/screens/Home.js
+++ sources/screens/Home.js
@@ -1,27 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import MapView from 'react-native-maps';
-
-
-const Home = () => {
-  return (
-    <MapView
-      style={{flex:1}}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-
-  );
-};
-
-Home.propTypes = {
-  navigation: PropTypes.shape({
-    navigate: PropTypes.func.isRequired,
-  }).isRequired,
-};
+import React from 'react';  
+import { StyleSheet, View } from 'react-native';  
+import MapView from 'react-native-maps';  
+import { Marker } from 'react-native-maps';  
+  
+  const Home = () => {
+    return (  
+      <View style={styles.MainContainer}>  
+  
+        <MapView  
+          style={styles.mapStyle}  
+          showsUserLocation={false}  
+          zoomEnabled={true}  
+          zoomControlEnabled={true}  
+          initialRegion={{  
+            latitude: 28.579660,   
+            longitude: 77.321110,  
+            latitudeDelta: 0.0922,  
+            longitudeDelta: 0.0421,  
+          }}>  
+  
+          <Marker  
+            coordinate={{ latitude: 28.579660, longitude: 77.321110 }}  
+            title={"JavaTpoint"}  
+            description={"Java Training Institute"}  
+          />  
+        </MapView>  
+          
+      </View>  
+    );  
+  }  
+  
+const styles = StyleSheet.create({  
+  MainContainer: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+    alignItems: 'center',  
+    justifyContent: 'flex-end',  
+  },  
+  mapStyle: {  
+    position: 'absolute',  
+    top: 0,  
+    left: 0,  
+    right: 0,  
+    bottom: 0,  
+  },  
+});  
 
 export default Home;
