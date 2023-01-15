1cc160d0a043e951619b1db3f1ce8bab26b6a9eb
Stooph-tech <alan-steeven.lam@epitech.eu>
2023-01-15 17:10:27 -0500

    add button take picture handling to ar screen

--- sources/screens/AR.js
+++ sources/screens/AR.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        paddingLeft: 130,
+        bottom: 0,
+        position: 'absolute',
+    }
+});
 
 const ARScreen = () => {
   const devices = useCameraDevices();
   const device = devices.back;
+  const camera = useRef(null);
+  const takePhoto = async () => {
+    try {
+      const takePhotoOptions = {
+        flash: 'on'
+      };
+      //Error Handle better
+      if (camera.current == null)
+        throw new Error('Camera Ref is Null');
+      console.log('Photo taking ....');
+      const photo = await camera.current.takePhoto(takePhotoOptions);
+      console.log(photo.path)
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   if (!device)
     return <View />;
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-    />
+    <>
+        <Camera style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          ref={camera}
+          photo={true}
+        />
+        <Button style={styles.buttonContainer}
+          onPress={takePhoto}
+          icon={() =>
+              <Image source={require('../images/white-circle.png')} />
+          }
+        />
+    </>
   );
 };
 
