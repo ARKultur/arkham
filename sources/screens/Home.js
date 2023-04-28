e8bc77bd9cbdf8a3b1267ecab8caf9c44b5d655c
Stooph-tech <alan-steeven.lam@epitech.eu>
2023-04-28 19:43:49 -0400

    Change mobile api key for geospatial and arcore

--- sources/screens/Home.js
+++ sources/screens/Home.js
@@ -7,53 +7,53 @@ import { get_markers } from '../reducers/Actions/markerAction';
 
 const Home = () => {
   const dispatch = useDispatch();
-  const state = useSelector(state => state.markerReducer);  
-  
+  const state = useSelector(state => state.markerReducer);
+
   useEffect (() => {
     if (state.markers.length == 0) {
       dispatch(get_markers())
     }
-  }, [state])   
+  }, [state])
 
   return (
-      <View style={styles.MapContainer}>  
-      <MapView  
+      <View style={styles.MapContainer}>
+      <MapView
         provider='google'
-        style={styles.mapStyle}  
-        showsUserLocation={true}  
-        zoomEnabled={true}  
-        zoomControlEnabled={true}  
-        initialRegion={{  
-          latitude: 46.8137431,   
-          longitude: -71.2084061,  
-          latitudeDelta: 0.0922,  
-          longitudeDelta: 0.0421,  
-        }}>  
-        { state.markers.length !== 0 ? 
+        style={styles.mapStyle}
+        showsUserLocation={true}
+        zoomEnabled={true}
+        zoomControlEnabled={true}
+        initialRegion={{
+          latitude: 46.8137431,
+          longitude: -71.2084061,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        { state.markers.length !== 0 ?
           state.markers.map((marker, index) => {
             console.log(marker);
             return (
-              <Marker  
+              <Marker
                 key={index}
-                coordinate={{ latitude: marker.latitude, longitude: marker.longitude}}  
-                title={marker.title}  
-                description={marker.description}  
+                coordinate={{ latitude: marker.latitude, longitude: marker.longitude}}
+                title={marker.title}
+                description={marker.description}
               />
             )
-          }) 
+          })
         :  <View/>}
-        
-      </MapView>  
-        
-      <Button onPress={() => console.log(state.markers)}>test</Button> 
 
-    </View> 
+      </MapView>
+
+      <Button onPress={() => console.log(state.markers)}>test</Button>
+
+    </View>
   )
 };
 
 
 const styles = StyleSheet.create({
-  
+
   MapContainer: {
     position: 'absolute',
     top: 50,
